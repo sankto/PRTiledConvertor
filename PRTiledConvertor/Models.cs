8bef02706b6527e0525a2a052c50941ec301ad86
Marc Dumoulin <mdumoulin.home@gmail.com>
2017-09-09 16:05:03 -0400

    Add project files.

---
+++ PRTiledConvertor/Models.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace PRTiledConvertor {
+	[Flags]
+	[JsonConverter(typeof(StringEnumConverter))]
+	public enum Direction : byte {
+		None = 0,
+		North = 1 << 0,
+		South = 1 << 1,
+		West = 1 << 2,
+		East = 1 << 3,
+		NorthWest = 1 << 4,
+		NorthEast = 1 << 5,
+		SouthWest = 1 << 6,
+		SouthEast = 1 << 7
+	};
+
+	class Mapping {
+		public const string COLLISION_SHEETID = "floors";
+		public const string GROUND_LAYER = "ground";
+		public const string DOODADS_LAYER = "doodads";
+		public const string NPCS_LAYER = "npcs";
+		public const string COLLISIONS_LAYER = "collisions";
+
+		[JsonProperty("SpriteSheets")]
+		public List<SpriteSheet> Sheets;
+		[JsonProperty("SpriteSets")]
+		public List<SpriteSet> Sets;
+
+		public static Dictionary<Tuple<int, int>, Direction> CollisionTiles = new Dictionary<Tuple<int, int>, Direction>(){
+			{ Tuple.Create(0, 0), Direction.North | Direction.West },
+			{ Tuple.Create(1, 0), Direction.North },
+			{ Tuple.Create(2, 0), Direction.North | Direction.East },
+			{ Tuple.Create(3, 0), Direction.North | Direction.West | Direction.East },
+			{ Tuple.Create(5, 0), Direction.North | Direction.West | Direction.East | Direction.South },
+
+			{ Tuple.Create(0, 1), Direction.West },
+			{ Tuple.Create(1, 1), Direction.None },
+			{ Tuple.Create(2, 1), Direction.East },
+			{ Tuple.Create(3, 1), Direction.West | Direction.East },
+			{ Tuple.Create(4, 1), Direction.North | Direction.West | Direction.South },
+			{ Tuple.Create(5, 1), Direction.North | Direction.South },
+			{ Tuple.Create(6, 1), Direction.North | Direction.East | Direction.South },
+
+			{ Tuple.Create(0, 2), Direction.West | Direction.South },
+			{ Tuple.Create(1, 2), Direction.South },
+			{ Tuple.Create(2, 2), Direction.East | Direction.South },
+			{ Tuple.Create(3, 2), Direction.South | Direction.West | Direction.East },
+		};
+
+		public SpriteSet FindSpriteset(Tile tile) {
+			if (tile != null)
+				foreach (var set in Sets) {
+					if (tile.SheetId == set.SheetId)
+						if (set.TileLocations.Any((t) => t.X == tile.X && t.Y == tile.Y))
+							return set;
+				}
+			return null;
+		}
+
+		public SpriteSheet FindSheet(Tile tile) {
+			if (tile == null)
+				return null;
+			return Sheets.FirstOrDefault((s) => s.Id == tile.SheetId);
+		}
+
+		public static Direction GetCollisionDirection(Tile tile) {
+			if (tile.SheetId == COLLISION_SHEETID) {
+				var result = from c in CollisionTiles
+							 where c.Key.Item1 == tile.X && c.Key.Item2 == tile.Y
+							 select c.Value;
+				if (result.Count() != 0)
+					return result.First();
+			}
+			return Direction.None;
+		}
+	}
+
+	class SpriteSheet {
+		public string Id;
+		public string File;
+		public int Width;
+		public int Height;
+		public string Style;
+		public bool MixWithOwnStyle;
+		public string LinkedSheetId;
+		public Dictionary<Direction, string> StyleSetByDirection;
+
+		private string FileName = null;
+		public string GetName() {
+			if (FileName == null)
+				FileName = Path.GetFileNameWithoutExtension(File);
+			return FileName;
+		}
+	}
+
+	class SpriteSet {
+		public string Id;
+		public string SheetId;
+		public List<SpriteSetTile> TileLocations;
+		public string RepresentSetId;
+
+		public SpriteSetTile GetFromStyle(string name) {
+			return TileLocations.FirstOrDefault((t) => t.Id == name);
+		}
+	}
+
+	class SpriteSetTile {
+		public string Id;
+		public int X;
+		public int Y;
+	}
+
+	class Tile {
+		public string SheetId;
+		public int X;
+		public int Y;
+	}
+
+	class NPC {
+		public string Id;
+		public Tile SheetTile;
+		public bool Flipped;
+		public int X;
+		public int Y;
+	}
+
+	class Map {
+		[JsonProperty("width")]
+		public int Width;
+		[JsonProperty("height")]
+		public int Height;
+		[JsonProperty("tilewidth")]
+		public int TileWidth;
+		[JsonProperty("tileheight")]
+		public int TileHeight;
+		[JsonProperty("tilesets")]
+		public List<MapTileset> Tilesets;
+		[JsonProperty("layers")]
+		public List<Layer> Layers;
+
+		public Tile GetTileFromGID(int gid, Mapping mapping) {
+			var set = Tilesets.First((s) => s.IsGIDWithin(gid));
+			var localGID = (gid - (set.FirstGID));
+			return new Tile() {
+				X = localGID % (set.ImageWidth / TileWidth),
+				Y = localGID / (set.ImageWidth / TileWidth),
+				SheetId = mapping.Sheets.First((s) => s.GetName() == set.Name).Id
+			};
+		}
+
+		public void InitTiles(Mapping mapping) {
+			foreach (var layer in Layers)
+				layer.InitTiles(this, mapping);
+		}
+
+		public Layer GetNPCLayer() {
+			return Layers.FirstOrDefault(layer => layer.Name == Mapping.NPCS_LAYER);
+		}
+
+		public Layer GetCollisionsLayer() {
+			return Layers.FirstOrDefault(layer => layer.Name == Mapping.COLLISIONS_LAYER);
+		}
+	}
+
+	class Layer {
+		[JsonProperty("width")]
+		public int Width;
+		[JsonProperty("height")]
+		public int Height;
+		[JsonProperty("data")]
+		public List<int> Data;
+		[JsonProperty("name")]
+		public string Name;
+		[JsonProperty("objects")]
+		public List<MapObject> Objects;
+
+		private List<Tile> tiles = null;
+		public List<Tile> Tiles { get { return tiles; } }
+		private List<Tile> modTiles = null;
+		public List<Tile> ModTiles { get { return modTiles; } }
+		private List<Direction> collisions = null;
+		public List<Direction> Collisions { get { return collisions; } }
+		private List<NPC> npcs = null;
+		public List<NPC> NPCs { get { return npcs; } }
+
+		public void InitTiles(Map map, Mapping mapping) {
+			if (tiles == null) {
+				tiles = new List<Tile>();
+				modTiles = new List<Tile>();
+				collisions = new List<Direction>();
+				npcs = new List<NPC>();
+				if (Data != null) {
+					for (int i = 0; i < Data.Count; i++) {
+						tiles.Add(Data[i] != 0 ? map.GetTileFromGID(Data[i], mapping) : null);
+						modTiles.Add(null);
+						if (Name == Mapping.COLLISIONS_LAYER) {
+							collisions.Add(tiles[i] == null ? Direction.None : Mapping.GetCollisionDirection(tiles[i]));
+						}
+					}
+				}
+			}
+		}
+
+		public bool IsWithinMap(int x, int y) {
+			return x >= 0 && y >= 0 && x < Width && y < Height;
+		}
+
+		public Tile GetTile(int x, int y) {
+			int i = (y * Width) + x;
+			return Tiles[i];
+		}
+
+		public void SetTile(int x, int y, Tile tile) {
+			int i = (y * Width) + x;
+			Tiles[i] = tile;
+		}
+
+		public Tile GetModTile(int x, int y) {
+			int i = (y * Width) + x;
+			return ModTiles[i];
+		}
+
+		public void SetModTile(int x, int y, Tile tile) {
+			int i = (y * Width) + x;
+			ModTiles[i] = tile;
+		}
+	}
+
+	class MapObject {
+		const int FLIPH_BIT = 31;
+		const int FLIPV_BIT = 30;
+		const int ROTATE_BIT = 29;
+
+		[JsonProperty("gid")]
+		public long GID;
+		[JsonProperty("properties")]
+		public ObjectProperties Properties;
+		[JsonProperty("x")]
+		public int X;
+		[JsonProperty("y")]
+		public int Y;
+
+		public string this[string propIndex] {
+			get { return Properties[propIndex]; }
+		}
+
+		public bool IsFlipped() {
+			var bits = new BitArray(BitConverter.GetBytes(GID));
+			return bits[FLIPH_BIT];
+		}
+
+		public int GetCleanGID() {
+			var bits = new BitArray(BitConverter.GetBytes(GID));
+			bits.Set(FLIPH_BIT, false);
+			bits.Set(FLIPV_BIT, false);
+			bits.Set(ROTATE_BIT, false);
+			var newgid = new byte[32]; // that's a Int32...
+			bits.CopyTo(newgid, 0);
+			return (int)BitConverter.ToInt64(newgid, 0); // now it's a long.
+		}
+	}
+
+	class ObjectProperties {
+		[JsonExtensionData]
+		private IDictionary<string, JToken> AdditionalData;
+
+		public string this[string index] {
+			get {
+				return AdditionalData[index].Value<string>();
+			}
+		}
+	}
+
+	class MapTileset {
+		[JsonProperty("firstgid")]
+		public int FirstGID;
+		[JsonProperty("imagewidth")]
+		public int ImageWidth;
+		[JsonProperty("imageheight")]
+		public int ImageHeight;
+		[JsonProperty("tilecount")]
+		public int TileCount;
+		[JsonProperty("name")]
+		public string Name;
+
+		public bool IsGIDWithin(int gid) {
+			return gid >= FirstGID && gid < FirstGID + TileCount;
+		}
+	}
+}
---
+++ PRTiledConvertor/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace PRTiledConvertor {
+	class Program {
+		public static Direction GetSimilarTiles(int posx, int posy, Layer layer, Mapping mapping) {
+			var tile = layer.GetTile(posx, posy);
+			var result = Direction.None;
+
+			if (tile == null) // null = empty tile
+				return result;
+
+			var spriteSet = mapping.FindSpriteset(tile);
+			var sheet = mapping.FindSheet(tile);
+
+			Tile t = null;
+			SpriteSet ts = null;
+			SpriteSheet tsh = null;
+
+			bool west = !layer.IsWithinMap(posx - 1, posy); // if west is outside
+			if (!west) {
+				t = layer.GetTile(posx - 1, posy);
+				ts = mapping.FindSpriteset(t);
+				tsh = mapping.FindSheet(t);
+				west = spriteSet == null && ts == null; // if west is empty AND we look for empty tile
+				west = west ? west : ts != null && spriteSet != null && tsh.Style == sheet.Style && // if west match style AND (if can mix with other spritesets OR west is the same spriteset)
+						(tsh.MixWithOwnStyle || (!tsh.MixWithOwnStyle && ts == spriteSet));
+			}
+
+			bool east = !layer.IsWithinMap(posx + 1, posy);
+			if (!east) {
+				t = layer.GetTile(posx + 1, posy);
+				ts = mapping.FindSpriteset(t);
+				tsh = mapping.FindSheet(t);
+				east = spriteSet == null && ts == null;
+				east = east ? east : ts != null && spriteSet != null && tsh.Style == sheet.Style &&
+						(tsh.MixWithOwnStyle || (!tsh.MixWithOwnStyle && ts == spriteSet));
+			}
+
+			bool north = !layer.IsWithinMap(posx, posy - 1);
+			if (!north) {
+				t = layer.GetTile(posx, posy - 1);
+				ts = mapping.FindSpriteset(t);
+				tsh = mapping.FindSheet(t);
+				north = spriteSet == null && ts == null;
+				north = north ? north : ts != null && spriteSet != null && tsh.Style == sheet.Style &&
+						(tsh.MixWithOwnStyle || (!tsh.MixWithOwnStyle && ts == spriteSet));
+			}
+
+			bool south = !layer.IsWithinMap(posx, posy + 1);
+			if (!south) {
+				t = layer.GetTile(posx, posy + 1);
+				ts = mapping.FindSpriteset(t);
+				tsh = mapping.FindSheet(t);
+				south = spriteSet == null && ts == null;
+				south = south ? south : ts != null && spriteSet != null && tsh.Style == sheet.Style &&
+						(tsh.MixWithOwnStyle || (!tsh.MixWithOwnStyle && ts == spriteSet));
+			}
+
+			if (west)
+				result |= Direction.West;
+			if (east)
+				result |= Direction.East;
+			if (north)
+				result |= Direction.North;
+			if (south)
+				result |= Direction.South;
+
+			return result;
+		}
+
+		public static void ProcessStyleable(Map map, Mapping mapping, Layer layer) {
+			// we go through each tile, left to right, top to bottom
+			for (int y = 0; y < map.Height; y++)
+				for (int x = 0; x < map.Width; x++) {
+					// get the tile that need to be observed
+					var tile = layer.GetTile(x, y);
+					// if this is left to True, we simply don't change the tile at all at the end
+					bool noModification = true;
+					// if the tile is empty
+					if (tile != null) {
+						// we get the sheet associated with the tile, from the 'mapping' source of all sheets.
+						// it's unlikely to be null.
+						var sheet = mapping.Sheets.FirstOrDefault((s) => s.Id == tile.SheetId);
+						// then we get the spriteset, if any, of the tile. Null if none associated.
+						var set = mapping.FindSpriteset(tile);
+						var initialStyle = set?.TileLocations.FirstOrDefault((t) => t.X == tile.X && t.Y == tile.Y).Id;
+						// if the spriteset is nonull and there is a style direction pattern associated with
+						// the sheet, and if the tile is the one being represented (ie. the tile == used in Tiled),
+						// the tile is elligible for transformation.
+						if (set != null && sheet.StyleSetByDirection != null && set.RepresentSetId == initialStyle) {
+							noModification = false;
+							// we get the directions of which the tile have compatible tiles to it,
+							// in the form a flagged enum.
+							var dirs = GetSimilarTiles(x, y, layer, mapping);
+							// we get the 'style' of the new tile, via the sheet pattern.
+							// style include IDs such as 'br' (bottom right), 'full', 'tlr' (top-left-right), etc.
+							// they can be a bit confusing.
+							var style = sheet.StyleSetByDirection[dirs];
+							// then we get the tile within the spriteset that is associated with that style id.
+							var setTile = set.GetFromStyle(style);
+							// The tile is now set to the new tile informations.
+							layer.SetModTile(x, y, new Tile() {
+								SheetId = tile.SheetId,
+								X = setTile.X,
+								Y = setTile.Y
+							});
+						}
+					}
+					if (noModification)
+						layer.SetModTile(x, y, tile);
+				}
+		}
+
+		public static void ProcessNPCs(Map map, Mapping mapping, Layer layer) {
+			foreach (var obj in layer.Objects) {
+				layer.NPCs.Add(new NPC() {
+					Id = obj["id"],
+					SheetTile = map.GetTileFromGID(obj.GetCleanGID(), mapping),
+					Flipped = obj.IsFlipped(),
+					X = obj.X / map.TileWidth,
+					Y = obj.Y / map.TileHeight
+				});
+			}
+		}
+
+		public static void ProcessMap(Map map, Mapping mapping) {
+			// we go through each layers of the map
+			foreach (var layer in map.Layers) {
+				if (layer.Name == Mapping.GROUND_LAYER || layer.Name == Mapping.DOODADS_LAYER) {
+					ProcessStyleable(map, mapping, layer);
+				} else if (layer.Name == Mapping.NPCS_LAYER) {
+					ProcessNPCs(map, mapping, layer);
+				}
+				// collisions are handled in classes Mapping and Layer
+			}
+		}
+
+		static void Main(string[] args) {
+			if (File.Exists(@"load.txt")) {
+				var paths = File.ReadAllLines(@"load.txt");
+				var mapped = JsonConvert.DeserializeObject<Mapping>(File.ReadAllText(paths[0]));
+				var map = JsonConvert.DeserializeObject<Map>(File.ReadAllText(paths[1]));
+
+				map.InitTiles(mapped);
+				ProcessMap(map, mapped);
+
+				File.WriteAllText(Path.GetDirectoryName(paths[1]) + @"\" + Path.GetFileNameWithoutExtension(paths[1]) + ".prmap", JsonConvert.SerializeObject(new {
+					Width = map.Width,
+					Height = map.Height,
+					TileWidth = map.TileWidth,
+					TileHeight = map.TileHeight,
+					Layers = from layer in map.Layers
+							 where layer.Name != Mapping.NPCS_LAYER &&
+								   layer.Name != Mapping.COLLISIONS_LAYER
+							 select new {
+							 	 Name = layer.Name,
+								 Tiles = from tile in layer.ModTiles
+										 select tile == null ? null : new {
+											 SId = tile.SheetId,
+											 SX = tile.X,
+											 SY = tile.Y
+										 }
+							 },
+					NPCs = map.GetNPCLayer()?.NPCs,
+					Collisions = map.GetCollisionsLayer()?.Collisions
+				}, Formatting.Indented, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Include }));
+			}
+			Console.WriteLine("--------------------------------");
+			Console.WriteLine("Press a key to exit.");
+			Console.ReadKey();
+		}
+	}
+}
